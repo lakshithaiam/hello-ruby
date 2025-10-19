57357ae96dd7be94d65ab78f3920f99b46432288
lakshithaiam <lakshithaiam@gmil.com>
2025-10-19 13:09:41 +0530

    add files

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'socket'
 
+# In-memory visitor counter
+$counter = 0
+
+# Route
 get '/' do
-  "Hello World from Ruby Sinatra!"
+  $counter += 1
+  hostname = Socket.gethostname
+  ruby_version = RUBY_VERSION
+  environment = ENV['RACK_ENV'] || "development"
+
+  <<~HTML
+    <h1>Hello from Ruby Sinatra 🚀</h1>
+    <p>Server Hostname: #{hostname}</p>
+    <p>Ruby Version: #{ruby_version}</p>
+    <p>Environment: #{environment}</p>
+    <p>You are visitor number: #{$counter}</p>
+  HTML
 end
